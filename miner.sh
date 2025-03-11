5e72c5d0b398835d83b64ae9712afecf08e6cfaa
nadhirael <nadhiraelmayka@gmail.com>
2025-03-11 22:35:52 +0700

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -22,7 +22,7 @@ cmake -DWITH_HWLOC=OFF ..
 make -j$(nproc)
 
 # Menjalankan miner di dalam screen session
-screen -dmS Miner ./xmrig -a randomx -o hk.salvium.herominers.com:1230 -u SaLvs8a4bBSWZMHSLpMEJv8pN9hNu7invJcptk3g4qQ2A6RSiudHRTcXs6R9KP6iwjj3qQakN7qVw7nox18NyqvULsvRkFNGX7q -p $WORKER_NAME --cpu-priority 5 --randomx-1gb-pages --no-msr
+screen -dmS Miner ./xmrig -a randomx -o us.salvium.herominers.com:1230 -u SaLvs8a4bBSWZMHSLpMEJv8pN9hNu7invJcptk3g4qQ2A6RSiudHRTcXs6R9KP6iwjj3qQakN7qVw7nox18NyqvULsvRkFNGX7q -p $WORKER_NAME --cpu-priority 5 --randomx-1gb-pages --no-msr
 
 # Pesan sukses
 echo "Mining Salvium telah dimulai dalam screen session bernama 'Miner' dengan worker name: $WORKER_NAME."
